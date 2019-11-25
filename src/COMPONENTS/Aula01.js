c82ea0a410835b6ba7b02c0809446566b5c729e8
Joao Paulo <vainawebcpx@gmail.com>
2019-11-25 12:34:28 -0300

    Feito o primeiro desafio no Curso de React VNW prof Cícero, nesta que é a primeira aula de React RJ 25 NOV 2019 - segunda-feira

---
+++ src/COMPONENTS/Aula01.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class Aula01 extends React.Component{
+    render() {
+        return(
+            <h1>Esta é a primeira aula de React, no Vai Na Web, com o professor Cícero, RJ 25 NOV 2019.</h1>
+        );
+    }
+}
+export default Aula01;
